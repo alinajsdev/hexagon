4919a52046cd90498b8622ce35f075f307a9b9e2
a-k-i-v-i <kiviasanova@gmail.com>
2023-08-12 05:44:14 -0700

    added router

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import "./App.css";
 import MotionWebLLS from "./MotionWebLLS";
 import RoboCarWash from "./RoboCarWash";
 import Contacts from "./Contacts";
+import { Box } from "@chakra-ui/react";
+import { Link} from "react-router-dom";
 
 function App() {
   const [state, setState] = useState(0);
@@ -33,7 +35,11 @@ function App() {
   }, []);
 
   return (
+ 
+
+
     <div id={"vector"}>
+
       <motion.div
   initial={{ y: '100%', opacity: 0 }}
   animate={{
@@ -63,6 +69,11 @@ function App() {
             </div>
           ))}
         </div>
+
+
+     
+  
+        <Box display={{md:"none", base:"block"}}>
         <div className="odd">
           {countArray.map((el, index) => (
             <div className="vector">
@@ -80,60 +91,65 @@ function App() {
         <div className="odd">
           {countArray.map((el, index) => (
             <div className="vector">
+              <Vector media={media} />
+            </div>
+          ))}
+        </div>
+        </Box>
+   <Box display={{md:"none", base:"block"}}>
+ 
+        <div className="even">
+          {countArray.map((el, index) => (
+            <div className="vector">
+              <Vector media={media} />
+            </div>
+          ))}
+        </div>
+   </Box>
+        <div className="odd">
+          {countArray.map((el, index) => (
+               <div className="vector">
               {index === 5 && state === 2 ? (
                 <motion.div
-                initial={{ scale: 0 }} 
-                animate={{ scale: 1 }} 
-                transition={{ duration: 1, ease: "anticipate" }} 
+                initial={{ scale: 0 }} // начальный масштаб блока
+                animate={{ scale: 1 }} // конечный масштаб блока
+                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
                 whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
                 style={{cursor : "pointer"}}
                 >
-                  <AboutUs media={media} />
+                  <Link to={'/about'}>
+                   <AboutUs media={media} />
+                  </Link>
+                 
                 </motion.div>
-              ) : (
-                <Vector media={media} />
-              )}
-            </div>
-          ))}
-        </div>
-        <div className="even">
-          {countArray.map((el, index) => (
-            <div className="vector">
-              <Vector media={media} />
-            </div>
-          ))}
-        </div>
-        <div className="odd">
-          {countArray.map((el, index) => (
-            <div className="vector">
-              {index === 4 && state === 2 ? (
+              ) : index === 4 && state === 2 ? (
                 <motion.div
-                initial={{ scale: 0 }} 
-                animate={{ scale: 1 }} 
-                transition={{ duration: 1, ease: "anticipate" }} 
-                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-                style={{cursor : "pointer"}}
-                >
-                  <SmartCards media={media} />
-                </motion.div>
-              ) : (
-                <Vector media={media} />
-              )}
-            </div>
-          ))}
-        </div>
-        <div className="even">
-          {countArray.map((el, index) => (
-            <div className="vector">
-              {index === 4 && state === 2 ? (
-                <motion.div
-                initial={{ scale: 0 }}
-                animate={{ scale: 1 }} 
-                transition={{ duration: 1, ease: "anticipate" }} 
+                initial={{ scale: 0 }} // начальный масштаб блока
+                animate={{ scale: 1 }} // конечный масштаб блока
+                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
                 whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
                 style={{cursor : "pointer"}}
                 >
                   <RoboBarista media={media} />
+                </motion.div>
+              ) : (
+                <Vector media={media} />
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="even">
+          {countArray.map((el, index) => (
+            <div className="vector">
+              {index === 4 && state === 2 ? (
+                <motion.div
+                initial={{ scale: 0 }} // начальный масштаб блока
+                animate={{ scale: 1 }} // конечный масштаб блока
+                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
+                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+                style={{cursor : "pointer"}}
+                >
+                  <SmartCards media={media} />
                 </motion.div>
               ) : (
                 <Vector media={media} />
@@ -154,6 +170,49 @@ function App() {
                 >
                   <GidroGel media={media} />
                 </motion.div>
+              ) :  (
+                <Vector media={media} />
+              )}
+            </div>
+          ))}
+        </div>
+
+     
+        <div className="even">
+          {countArray.map((el, index) => (
+            <div className="vector">
+              {index === 4 && state === 2 ? (
+                <motion.div
+                initial={{ scale: 0 }}
+                animate={{ scale: 1 }} 
+                transition={{ duration: 1, ease: "anticipate" }} 
+                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+                style={{cursor : "pointer"}}
+                >
+                  <RoboCarWash media={media} />
+                </motion.div>
+              ) : (
+                <Vector media={media} />
+              )}
+            </div>
+          ))}
+        </div>
+        <div className="odd">
+          {countArray.map((el, index) => (
+            <div className="vector">
+              {index === 5 && state === 2 ? (
+                <motion.div
+                initial={{ scale: 0 }} // начальный масштаб блока
+                animate={{ scale: 1 }} // конечный масштаб блока
+                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
+                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+                style={{cursor : "pointer"}}
+                >
+                  <Link to={'/contacts'}>
+                    <Contacts media={media} />
+                  </Link>
+                
+                </motion.div>
               ) : index === 4 && state === 2 ? (
                 <motion.div
                 initial={{ scale: 0 }} // начальный масштаб блока
@@ -173,43 +232,6 @@ function App() {
         <div className="even">
           {countArray.map((el, index) => (
             <div className="vector">
-              {index === 4 && state === 2 ? (
-                <motion.div
-                initial={{ scale: 0 }} // начальный масштаб блока
-                animate={{ scale: 1 }} // конечный масштаб блока
-                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
-                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-                style={{cursor : "pointer"}}
-                >
-                  <RoboCarWash media={media} />
-                </motion.div>
-              ) : (
-                <Vector media={media} />
-              )}
-            </div>
-          ))}
-        </div>
-        <div className="odd">
-          {countArray.map((el, index) => (
-            <div className="vector">
-              {index === 4 && state === 2 ? (
-                <motion.div
-                initial={{ scale: 0 }} // начальный масштаб блока
-                animate={{ scale: 1 }} // конечный масштаб блока
-                transition={{ duration: 1, ease: "anticipate" }} // начальный масштаб блока
-                whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-                style={{ cursor: "pointer" }}>
-                  <Contacts media={media} />
-                </motion.div>
-              ) : (
-                <Vector media={media} />
-              )}
-            </div>
-          ))}
-        </div>
-        <div className="even">
-          {countArray.map((el, index) => (
-            <div className="vector">
               <Vector media={media} />
             </div>
           ))}
@@ -256,13 +278,7 @@ function App() {
             </div>
           ))}
         </div>
-        <div className="odd">
-          {countArray.map((el, index) => (
-            <div className="vector">
-              <Vector media={media} />
-            </div>
-          ))}
-        </div>
+  
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { BrowserRouter } from 'react-router-dom';
+import Main from './Router/Main';
 
+const breakpoints = {
+  sm: '320px',
+  md: '768px',
+  lg: '960px',
+  xl: '1200px',
+  '2xl': '1536px',
+}
+const theme = extendTheme({ breakpoints })
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+     <BrowserRouter>
+     <Main/>
+     </BrowserRouter>
+    </ChakraProvider>
+    
   </React.StrictMode>
 );
 
